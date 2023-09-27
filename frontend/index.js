ede98aec7838a0771351f0af15290becc507e08e
cotyadams <cotyadams1@gmail.com>
2023-09-27 17:50:36 -0400

    task 2 complete

--- frontend/index.js
+++ frontend/index.js
@@ -7,14 +7,43 @@ function moduleProject1() {
 
   // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
   //  ✨ add your code here
-  const widget = document.body.getElementsByTagName('section')[0];
-  widget.classList.add('widget');
+  let widgets = document.body.querySelectorAll('div');
+  widgets.forEach(widget => {
+    widget.classList.add('widget');
+  })
+  let is_orange = false;
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
+  //create the quote
+  let widget = document.getElementsByClassName('widget')[0];
+  console.log(widget)
+  let header = widget.getElementsByTagName('h3')[0];
+  console.log(header)
+  widget.addEventListener('click', () => {
+    if (!is_orange) {
+      header.style.color = '#ff4b00';
+    }
+    else {
+      header.style.color = 'rgb(133, 133, 133)';
+    }
+    is_orange = !is_orange;
+  })
+  let text = document.createElement('p');
+  text.id = 'body_quote';
+  // add content to quote
+  text.textContent = 'All our dreams can come true, if we have the courage to pursue them.'
+  widget.appendChild(text);
+  //create author
+  let author = document.createElement('p');
+  author.id = 'author';
+  author.textContent = '--Walt Disney';
+  author.style.width = '100%'
+  author.style.textAlign = 'right';
+  console.log(author)
+  widget.appendChild(author);
   
-  // 👉 TASK 3 - Build a "Corporate Speak" widget
   //  ✨ add your code here
-
+  
   // 👉 TASK 4 - Build a "Countdown" widget
   //  ✨ add your code here
 
@@ -24,6 +53,8 @@ function moduleProject1() {
   // 👉 TASK 6 - Make it so user can tab through the widgets
   //  ✨ add your code here
 
+
+  
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
